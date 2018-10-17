18fbc934200ada82e8c74d7274b0ec9118ce14a1
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-10-17 04:30:01 +0000

    apply fac

--- launch/NASCO_fac.sh
+++ launch/NASCO_fac.sh
@@ -1,5 +1,5 @@
-export ROS_IP = 192.168.100.183
+export ROS_IP = 192.168.100.150
 export ROS_MASTER_URI=http://192.168.100.183:11311
 source /opt/ros/melodic/setup.bash
 source /home/amigos/ros/devel/setup.bash
-roslaunch nasco_system nasco.launch
+roslaunch nasco_system nasco_fac.launch
