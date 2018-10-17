4df621e49b25f26346120a98ac17e8ae82e8a1d7
sakasaikeisuke <sakasai@a.phys.nagoya-u.ac.jp>
2018-10-17 11:04:32 +0000

    fix

--- launch/NASCO_fac.sh
+++ launch/NASCO_fac.sh
@@ -1,4 +1,4 @@
-export ROS_IP = 192.168.100.150
+export ROS_IP=192.168.100.150
 export ROS_MASTER_URI=http://192.168.100.183:11311
 source /opt/ros/melodic/setup.bash
 source /home/necst/ros/devel/setup.bash
