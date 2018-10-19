42b363093ec7e51fa15806b7dc681e831874c1a0
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-10-19 06:24:51 +0000

    add slider

---
+++ scripts/slider_controller.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+name = "slider_controller"
+
+import numpy
+import time
+
+
+class slider(object):
+
+    def __init__(self):
+        pass
+
+    def set_length(self, len):
+        ~~~
+
+        return
