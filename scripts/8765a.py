91dd66262030a494c1c429c6977343b5a51ae105
k.noda <k.noda@a.phys.nagoya-u.ac.jp>
2018-11-02 18:24:49 +0900

    for 16 switch

--- scripts/8765a.py
+++ scripts/8765a.py
@@ -10,65 +10,39 @@ name = "switch_8765a"
 
 class A8765(object):
 
-    def __init__(self, ip, port, ch = '1X'):
+    def __init__(self, ip, port):
         self.pa = A11713C.a11713c(ip, port)
-        self.ch = ch
+        self.ch_list = ['1X', '1Y', '2X', '2Y']
+        self.ch = rospy.get_param('~ch')
 
         self.pub_level = rospy.Publisher(
-	        name = '/8765a_level',
-		data_class = std_msgs.msg.Bool,
-		latch = True,
-		queue_size = 1
-		)
-
-        self.pub_vol = rospy.Publisher(
-	        name = '/8765a_voltage',
-		data_class = std_msgs.msg.String,
-		latch = True,
-		queue_size = 1
-		)
+            name = '/8765a_level_{}'.format(self.ch),
+            data_class = std_msgs.msg.Int8,
+            latch = True,
+            queue_size = 1
+        )
 
         self.sub_level = rospy.Subscriber(
-	        name = '/8765a_level_cmd',
-		data_class = std_msgs.msg.Bool,
-		callback = self.callback_level,
-		queue_size = 1,
-		)
-
-        self.sub_vol = rospy.Subscriber(
-	        name = '/8765a_voltage_cmd',
-		data_class = std_msgs.msg.String,
-		callback = self.callback_vol,
-		queue_size = 1,
-		)
-
-        self.pub_function_level()
-        self.pub_function_vol()
+            name = '/8765a_level_cmd_{}'.format(self.ch),
+            data_class = std_msgs.msg.Int8,
+            callback = self.callback,
+            queue_size = 1,
+        )
+        
+        self.pub_function()
         pass
 
-    def callback_level(self, status):
+    def callback(self, status):
         self.cmd = status.data
-
-        if self.cmd == True: # NASCO
-            self.pa.set_level(level = 1, ch = self.ch)
-        elif self.cmd == False: # SMART
-            self.pa.set_level(level = 0, ch = self.ch)
-        self.pub_function_level()
+        
+        self.pa.set_level(level = self.cmd, ch = self.ch)
+        
+        self.pub_function()
         return
 
-    def callback_vol(self, status):
-        self.pa.set_voltage(status.data, bank=1)
-        self.pub_function_vol()
-        return
-
-    def pub_function_level(self):
+    def pub_function(self):
         mode = self.pa.query_level()
-        self.pub_level.publish(mode[0])
-        return
-
-    def pub_function_vol(self):
-        mode = self.pa.query_voltage()
-        self.pub_vol.publish(mode[0])
+        self.pub_level.publish(mode[self.ch_list.index(self.ch)])
         return
 
 
