ce370f2e748788fdd04cf96ed810c684a729915e
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-03 08:58:15 +0000

    web_monitor

---
+++ scripts/topic_webserver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rospy, os, ssl
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+def kill():
+    os.system('kill -KILL ' + str(os.getpid()))
+
+os.chdir('/home/amigos/ros/src/nasco_system/web_monitor')
+host = 'localhost'
+port = 10000
+httpd = HTTPServer(('', port), SimpleHTTPRequestHandler)
+print('serving at port', port)
+httpd.serve_forever()
+
+rospy.init_node('webserver')
+rospy.on_shutdown(kill)
