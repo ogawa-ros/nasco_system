d8d2bb8136d6ddace1a0e0e4bb0500c6c3f001c2
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-03 18:23:25 +0900

    monitor

---
+++ web_monitor/subscriber.js
@@ -0,0 +1,20 @@
+function Listerner (name, msg) {
+    this.name = name;
+    this.age = age;
+    this.ros = new ROSLIB.Ros();
+
+    var sub = new ROSLIB.Topic(
+        {
+        ros : this.ros,
+        name : name,
+        messageType : 'std_msgs/' + msg,
+        }
+    );
+
+    sub.subscribe(function(message) {
+        var res = message.data;
+        var el = document.createElement(name);
+        el.innerHTML = res
+        document.getElementById(name+'_value').appendChild(el);
+    }
+                 );
