2839f23d9d7008bd91ea7ce4853291b3d2055210
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-03 20:15:52 +0900

    monitor

--- web_monitor/monitor.js
+++ web_monitor/monitor.js
@@ -129,3 +129,126 @@ sis_vol_1lu.subscribe(function(message) {
 
 <!-- sis_vol -->
 
+<!-- sis_cur -->
+
+var sis_cur_2l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_2l",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_2r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_2r",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_3l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_3l",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_3r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_3r",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_4l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_4l",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_4r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_4r",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_5l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_5l",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_5r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_5r",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_1rl = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_1rl",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_1ru = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_1ru",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_1ll = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_1ll",
+    messageType : "std_msgs/Float64"
+});
+
+var sis_cur_1lu = new ROSLIB.Topic({
+    ros : ros,
+    name : "/sis_cur_1lu",
+    messageType : "std_msgs/Float64"
+});
+
+sis_cur_2l.subscribe(function(message) {
+    document.getElementById("sis_cur_2l").innerHTML = message.data;
+});
+
+sis_cur_2r.subscribe(function(message) {
+    document.getElementById("sis_cur_2r").innerHTML = message.data;
+});
+
+sis_cur_3l.subscribe(function(message) {
+    document.getElementById("sis_cur_3l").innerHTML = message.data;
+});
+
+sis_cur_3r.subscribe(function(message) {
+    document.getElementById("sis_cur_3r").innerHTML = message.data;
+});
+
+sis_cur_4l.subscribe(function(message) {
+    document.getElementById("sis_cur_4l").innerHTML = message.data;
+});
+
+sis_cur_4r.subscribe(function(message) {
+    document.getElementById("sis_cur_4r").innerHTML = message.data;
+});
+
+sis_cur_5l.subscribe(function(message) {
+    document.getElementById("sis_cur_5l").innerHTML = message.data;
+});
+
+sis_cur_5r.subscribe(function(message) {
+    document.getElementById("sis_cur_5r").innerHTML = message.data;
+});
+
+sis_cur_1rl.subscribe(function(message) {
+    document.getElementById("sis_cur_1rl").innerHTML = message.data;
+});
+
+sis_cur_1ru.subscribe(function(message) {
+    document.getElementById("sis_cur_1ru").innerHTML = message.data;
+});
+
+sis_cur_1ll.subscribe(function(message) {
+    document.getElementById("sis_cur_1ll").innerHTML = message.data;
+});
+
+sis_cur_1lu.subscribe(function(message) {
+    document.getElementById("sis_cur_1lu").innerHTML = message.data;
+})
+
+<!-- sis_cur -->
