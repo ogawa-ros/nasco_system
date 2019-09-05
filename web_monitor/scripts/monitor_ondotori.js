98ed04c21b8b47b27022cbe713d725058e559333
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-05 21:07:35 +0900

    info : web におんどとり追加

---
+++ web_monitor/scripts/monitor_ondotori.js
@@ -0,0 +1,39 @@
+var ros = new ROSLIB.Ros({url : "ws://" + "172.20.0.233" + ":9000"});
+
+ros.on("connection", function() {console.log("websocket: connected"); });
+ros.on("error", function(error) {console.log("websocket error; ", error); });
+ros.on("close", function() {console.log("websocket: closed");});
+
+var tr71w_1_temp_ch1 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/tr71w_1_temp_ch1",
+    messageType : "std_msgs/Int32"
+});
+
+var tr71w_2_temp_ch1 = new ROSLIB.Topic({
+    ros : ros,
+    name : "tr71w_2_temp_ch1",
+    messageType : "std_msgs/Int32"
+});
+
+var patt_3l = new ROSLIB.Topic({
+    ros : ros,
+    name : "tr71w_3_temp_ch1",
+    messageType : "std_msgs/Int32"
+});
+
+
+tr71w_1_temp_ch1.subscribe(function(message) {
+    document.getElementById("tr71w_1_temp_ch1").innerHTML = (message.data);
+});
+
+tr71w_2_temp_ch1.subscribe(function(message) {
+    document.getElementById("tr71w_2_temp_ch1").innerHTML = (message.data);
+});
+
+tr71w_3_temp_ch1.subscribe(function(message) {
+    document.getElementById("tr71w_3_temp_ch1").innerHTML = (message.data);
+});
+
+
+
