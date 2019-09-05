c655b2cb4e6b44aba9053ca8a90caf8552edfb1b
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-05 19:19:21 +0900

    info : Prog-att 完成 ( web にも埋め込んだ )

---
+++ web_monitor/scripts/monitor_patt.js
@@ -0,0 +1,130 @@
+var ros = new ROSLIB.Ros({url : "ws://" + "172.20.0.233" + ":9000"});
+
+ros.on("connection", function() {console.log("websocket: connected"); });
+ros.on("error", function(error) {console.log("websocket error; ", error); });
+ros.on("close", function() {console.log("websocket: closed");});
+
+
+<!-- patt -->
+
+var patt_2l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_2l",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_2r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_2r",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_3l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_3l",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_3r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_3r",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_4l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_4l",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_4r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_4r",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_5l = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_5l",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_5r = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_5r",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_1rl = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_1rl",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_1ru = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_1ru",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_1ll = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_1ll",
+    messageType : "std_msgs/Float64"
+});
+
+var patt_1lu = new ROSLIB.Topic({
+    ros : ros,
+    name : "/patt_1lu",
+    messageType : "std_msgs/Float64"
+});
+
+patt_2l.subscribe(function(message) {
+    document.getElementById("patt_2l").innerHTML = (message.data).toFixed(1);
+});
+
+patt_2r.subscribe(function(message) {
+    document.getElementById("patt_2r").innerHTML = (message.data).toFixed(1);
+});
+
+patt_3l.subscribe(function(message) {
+    document.getElementById("patt_3l").innerHTML = (message.data).toFixed(1);
+});
+
+patt_3r.subscribe(function(message) {
+    document.getElementById("patt_3r").innerHTML = (message.data).toFixed(1);
+});
+
+patt_4l.subscribe(function(message) {
+    document.getElementById("patt_4l").innerHTML = (message.data).toFixed(1);
+});
+
+patt_4r.subscribe(function(message) {
+    document.getElementById("patt_4r").innerHTML = (message.data).toFixed(1);
+});
+
+patt_5l.subscribe(function(message) {
+    document.getElementById("patt_5l").innerHTML = (message.data).toFixed(1);
+});
+
+patt_5r.subscribe(function(message) {
+    document.getElementById("patt_5r").innerHTML = (message.data).toFixed(1);
+});
+
+patt_1rl.subscribe(function(message) {
+    document.getElementById("patt_1rl").innerHTML = (message.data).toFixed(1);
+});
+
+patt_1ru.subscribe(function(message) {
+    document.getElementById("patt_1ru").innerHTML = (message.data).toFixed(1);
+});
+
+patt_1ll.subscribe(function(message) {
+    document.getElementById("patt_1ll").innerHTML = (message.data).toFixed(1);
+});
+
+patt_1lu.subscribe(function(message) {
+    document.getElementById("patt_1lu").innerHTML = (message.data).toFixed(1);
+})
+
+<!-- patt -->
