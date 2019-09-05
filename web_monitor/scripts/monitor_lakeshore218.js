8fdaeb2dc26ea79c8a9d131d32a84035a395c2d9
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-05 21:17:59 +0900

    info : web に lakeshore218 を追加

---
+++ web_monitor/scripts/monitor_lakeshore218.js
@@ -0,0 +1,86 @@
+var ros = new ROSLIB.Ros({url : "ws://" + "172.20.0.233" + ":9000"});
+
+ros.on("connection", function() {console.log("websocket: connected"); });
+ros.on("error", function(error) {console.log("websocket error; ", error); });
+ros.on("close", function() {console.log("websocket: closed");});
+
+var lakeshore_ch1 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch1",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch2 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch2",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch3 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch3",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch4 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch4",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch5 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch5",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch6 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch6",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch7 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch7",
+    messageType : "std_msgs/Float64"
+});
+
+var lakeshore_ch8 = new ROSLIB.Topic({
+    ros : ros,
+    name : "/lakeshore_ch",
+    messageType : "std_msgs/Float64"
+});
+
+
+lakeshore_ch1.subscribe(function(message) {
+    document.getElementById("lakeshore_ch1").innerHTML = (message.data);
+});
+
+lakeshore_ch2.subscribe(function(message) {
+    document.getElementById("lakeshore_ch2").innerHTML = (message.data);
+});
+
+lakeshore_ch3.subscribe(function(message) {
+    document.getElementById("lakeshore_ch3").innerHTML = (message.data);
+});
+
+lakeshore_ch4.subscribe(function(message) {
+    document.getElementById("lakeshore_ch4").innerHTML = (message.data);
+});
+
+lakeshore_ch5.subscribe(function(message) {
+    document.getElementById("lakeshore_ch5").innerHTML = (message.data);
+});
+
+lakeshore_ch6.subscribe(function(message) {
+    document.getElementById("lakeshore_ch6").innerHTML = (message.data);
+});
+
+lakeshore_ch7.subscribe(function(message) {
+    document.getElementById("lakeshore_ch7").innerHTML = (message.data);
+});
+
+lakeshore_ch8.subscribe(function(message) {
+    document.getElementById("lakeshore_ch8").innerHTML = (message.data);
+});
