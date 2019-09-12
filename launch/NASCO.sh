219b561b9b4db3bec87d264f9e4033fe850978b2
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-12 05:27:30 +0000

    info : temp change

--- launch/NASCO.sh
+++ launch/NASCO.sh
@@ -1,7 +1,8 @@
 #export ROS_IP=192.168.101.170
 #export ROS_MASTER_URI=http://192.168.101.170:11311
-export ROS_IP=172.20.0.233
+export ROS_IP=172.20.0.237
 export ROS_MASTER_URI=http://172.20.0.21:11311
+
 
 source /opt/ros/melodic/setup.bash
 source /home/amigos/ros/devel/setup.bash
