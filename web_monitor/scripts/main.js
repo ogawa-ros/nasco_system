05cf3005b6c2269a7888b53d46d42d3b931c40ff
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-12 23:50:55 +0900

    info : websocket の再接続対応

--- web_monitor/scripts/main.js
+++ web_monitor/scripts/main.js
@@ -1,10 +1,3 @@
-var ros = new ROSLIB.Ros({url : 'ws://' + IP_rxobs + ':9000'});
-
-ros.on('connection', function() {console.log('websocket: connected'); });
-ros.on('error', function(error) {console.log('websocket error; ', error); });
-ros.on('close', function() {console.log('websocket: closed');});
-
-
 function sleep(waitsecond, callback){
     var time_count = 0;
     var id = setInterval(function(){
