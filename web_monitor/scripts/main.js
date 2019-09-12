5c72be249ef5ad3e592bb3c6a89b48daa45be7e9
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-12 17:52:30 +0900

    info : bugfix

--- web_monitor/scripts/main.js
+++ web_monitor/scripts/main.js
@@ -1,4 +1,4 @@
-var ros = new ROSLIB.Ros({url : 'ws://' + '172.20.0.237' + ':9000'});
+var ros = new ROSLIB.Ros({url : 'ws://' + IP_rxobs + ':9000'});
 
 ros.on('connection', function() {console.log('websocket: connected'); });
 ros.on('error', function(error) {console.log('websocket error; ', error); });
