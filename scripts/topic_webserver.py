2448da2edbf88a5c110df93bd582e463c7b7ab11
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-12 17:16:39 +0900

    info : bugfix

--- scripts/topic_webserver.py
+++ scripts/topic_webserver.py
@@ -7,7 +7,6 @@ def kill():
     os.system('kill -KILL ' + str(os.getpid()))
 
 os.chdir('/home/amigos/ros/src/nasco_system/web_monitor')
- host = '172.20.0.237'
 host = rospy.get_params('~host')
 port = rospy.get_params('~port')
 httpd = HTTPServer((host, port), SimpleHTTPRequestHandler)
