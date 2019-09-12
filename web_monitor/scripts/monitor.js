8af8c1db46694d896a9136a785229433194a300f
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-12 17:38:46 +0900

    info : bugfix

--- web_monitor/scripts/monitor.js
+++
@@ -1,7 +0,0 @@
-var ros = new ROSLIB.Ros({url : "ws://" + "172.20.0.237" + ":9000"});
-
-ros.on("connection", function() {console.log("websocket: connected"); });
-ros.on("error", function(error) {console.log("websocket error; ", error); });
-ros.on("close", function() {console.log("websocket: closed");});
-
-document.write("<script type='text/javascript' src='./monitor_lakeshore218.js'<>\/script>");
