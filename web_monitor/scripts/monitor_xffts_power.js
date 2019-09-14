f7933e90bcd289450a41e819fe18bb55f8e7972c
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-15 06:15:06 +0900

    info : update

--- web_monitor/scripts/monitor_xffts_power.js
+++ web_monitor/scripts/monitor_xffts_power.js
@@ -7,98 +7,98 @@ ros.on("close", function() {console.log("websocket: closed");});
 
 var xffts_power_board01 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board01",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board01s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board02 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board02",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board02s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board03 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board03",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board03s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board04 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board04",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board04s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board05 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board05",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board05s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board06 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board06",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board06s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board07 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board07",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board07s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board08 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board08",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board08s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board09 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board09",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board09s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board10 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board10",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board10s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board11 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board11",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board11s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board12 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board12",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board12s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board13 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board13",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board13s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board14 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board14",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board14s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board15 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board15",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board15s",
+    messageType : "std_msgs/Float64"
 });
 
 var xffts_power_board16 = new ROSLIB.Topic({
     ros : ros,
-    name : "/xffts_power_board16",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    name : "/xffts_power_board16s",
+    messageType : "std_msgs/Float64"
 });
 
 
@@ -109,7 +109,7 @@ xffts_power_board01.subscribe(function(message) {
 
 xffts_power_board02.subscribe(function(message) {
     document.getElementById("xffts_power_board02").innerHTML = Math.log10(message.total_power).toFixed(3);
-    sleep(3000);    
+    sleep(3000);
 });
 
 xffts_power_board03.subscribe(function(message) {
