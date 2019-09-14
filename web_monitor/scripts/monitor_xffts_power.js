e00c9a60e2410743b8a906eb8c06223f145cb3d2
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-15 01:58:09 +0900

    info : power を追加

--- web_monitor/scripts/monitor_xffts_power.js
+++ web_monitor/scripts/monitor_xffts_power.js
@@ -8,97 +8,97 @@ ros.on("close", function() {console.log("websocket: closed");});
 var xffts_power_board01 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board01",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board02 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board02",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board03 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board03",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board04 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board04",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board05 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board05",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board06 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board06",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board07 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board07",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board08 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board08",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board09 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board09",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board10 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board10",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board11 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board11",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board12 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board12",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board13 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board13",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board14 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board14",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board15 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board15",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 var xffts_power_board16 = new ROSLIB.Topic({
     ros : ros,
     name : "/xffts_power_board16",
-    messageType : "nascorx_xffts/XFFTS_totalp_msg"
+    messageType : "nasco_system/XFFTS_totalp_msg"
 });
 
 
