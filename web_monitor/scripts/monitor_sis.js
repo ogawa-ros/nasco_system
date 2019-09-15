bb4cf04dd5f1db1432fea7d9eef6fd38000f1035
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-15 15:26:20 +0900

    info : web monitor

--- web_monitor/scripts/monitor_sis.js
+++ web_monitor/scripts/monitor_sis.js
@@ -79,15 +79,13 @@ var sis_vol_1lu = new ROSLIB.Topic({
     messageType : "std_msgs/Float64"
 });
 
-setTimeout(
-    sis_vol_2l.subscribe(function(message) {
+sis_vol_2l.subscribe(function(message) {
         document.getElementById("sis_vol_2l").innerHTML = (message.data).toFixed(3);
-    }), 5000);
+});
 
-setTimeout(
-    sis_vol_2r.subscribe(function(message) {
-        document.getElementById("sis_vol_2r").innerHTML = (message.data).toFixed(3);
-    }), 5000);
+sis_vol_2r.subscribe(function(message) {
+    document.getElementById("sis_vol_2r").innerHTML = (message.data).toFixed(3);
+});
 
 sis_vol_3l.subscribe(function(message) {
     document.getElementById("sis_vol_3l").innerHTML = (message.data).toFixed(3);
