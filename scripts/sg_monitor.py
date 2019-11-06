2326a65d72f05a144621fac7919c498b7f90813a
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-11-06 19:27:12 +0900

    info : minor change

---
+++ scripts/sg_monitor.py
@@ -0,0 +1,133 @@
+import time
+
+import rospy
+from std_msgs.msg import Int32
+from std_msgs.msg import Float64
+from std_msgs.msg import String
+
+name = 'sg_monitor'
+rospy.init_node(name)
+
+class sg_monitor(object):
+
+    def __init__(self)
+        mode_list = ['1st', '2nd_upper', '2nd_lower']
+
+        reta = 1.
+        rate_pub = 1e-2
+        self.sg_100ghz_freq = [0., 0., 0.]
+        self.sg_100ghz_power = [0., 0., 0.]
+        self.sg_100ghz_onoff = [0, 0, 0]
+        self.sg_200ghz_freq = [0., 0., 0.]
+        self.sg_200ghz_power = [0., 0., 0.]
+        self.sg_200ghz_onoff = [0, 0, 0]
+
+        self.pub_sg_100ghz_freq = [
+            rospy.Publisher('sg_100ghz_{}_freq_web'.format(_mode), Float64, queue_size=1)
+            for _mode in mode_list]
+        self.pub_sg_100ghz_power = [
+            rospy.Publisher('sg_100ghz_{}_power_web'.format(_mode), Float64, queue_size=1)
+            for _mode in mode_list]
+        self.pub_sg_100ghz_onoff = [
+            rospy.Publisher('sg_100ghz_{}_onoff_web'.format(_mode), String, queue_size=1)
+            for _mode in mode_list]
+
+        self.sub_sg_100ghz_freq = [
+            rospy.Publisher('sg_100ghz_{}_freq'.format(_mode), Float64, callback=self.set_100ghz_freq, callback_args=_mode)
+            for _mode in mode_list]
+        self.sub_sg_100ghz_power = [
+            rospy.Publisher('sg_100ghz_{}_power'.format(_mode), Float64, callback=self.set_100ghz_power, callback_args=_mode)
+            for _mode in mode_list]
+        self.sub_sg_100ghz_onoff = [
+            rospy.Publisher('sg_100ghz_{}_onoff'.format(_mode), String, callback=self.set_100ghz_onoff, callback_args=_mode)
+            for _mode in mode_list]
+
+        self.pub_sg_200ghz_freq = [
+            rospy.Publisher('sg_200ghz_{}_freq_web'.format(_mode), Float64, queue_size=1)
+            for _mode in mode_list]
+        self.pub_sg_200ghz_power = [
+            rospy.Publisher('sg_200ghz_{}_power_web'.format(_mode), Float64, queue_size=1)
+            for _mode in mode_list]
+        self.pub_sg_200ghz_onoff = [
+            rospy.Publisher('sg_200ghz_{}_onoff_web'.format(_mode), Int32, queue_size=1)
+            for _mode in mode_list]
+
+        self.sub_sg_200ghz_freq = [
+            rospy.Publisher('sg_200ghz_{}_freq'.format(_mode), Float64, callback=self.set_200ghz_freq, callback_args=_mode)
+            for _mode in mode_list]
+        self.sub_sg_200ghz_power = [
+            rospy.Publisher('sg_200ghz_{}_power'.format(_mode), Float64, callback=self.set_200ghz_power, callback_args=_mode)
+            for _mode in mode_list]
+        self.sub_sg_200ghz_onoff = [
+            rospy.Publisher('sg_200ghz_{}_onoff'.format(_mode), Int32, callback=self.set_200ghz_onoff, callback_args=_mode)
+            for _mode in mode_list]
+
+    def set_100ghz_freq(self, freq=0., mode):
+        idx = mode_list.index(mode)
+        self.sg_100ghz_freq[idx] = freq
+
+    def set_100ghz_power(self, power=0., mode):
+        idx = mode_list.index(mode)
+        self.sg_100ghz_power[idx] = power
+
+    def set_100ghz_onoff(self, onoff=0., mode):
+        idx = mode_list.index(mode)
+        self.sg_100ghz_onoff[idx] = onoff
+
+    def set_200ghz_freq(self, freq=0., mode):
+        idx = mode_list.index(mode)
+        self.sg_200ghz_freq[idx] = freq
+
+    def set_200ghz_power(self, power=0., mode):
+        idx = mode_list.index(mode)
+        self.sg_200ghz_power[idx] = power
+
+    def set_200ghz_onoff(self, onoff=0., mode):
+        idx = mode_list.index(mode)
+        self.sg_200ghz_onoff[idx] = ofoff
+
+    def web_100ghz_freq(self):
+        for i, pub in enumerate(self.pub_sg_100ghz_freq):
+            pub.publish(self.sg_100ghz_freq[i])
+            time.sleep(rate_pub)
+        time.sleep(rate)
+
+    def web_100ghz_power(self):
+        for i, pub in enumerate(self.pub_sg_100ghz_power):
+            pub.publish(self.sg_100ghz_power[i])
+            time.sleep(rate_pub)
+        time.sleep(rate)
+
+    def web_100ghz_onoff(self):
+        for i, pub in enumerate(self.pub_sg_100ghz_onoff):
+            if self.sg_100ghz_onoff[i] == 0: onoff = 'ON'
+            else: onoff = 'OFF'
+            pub.publish(onoff)
+            time.sleep(rate_pub)
+        time.sleep(rate)
+
+    def web_200ghz_freq(self):
+        for i, pub in enumerate(self.pub_sg_200ghz_freq):
+            pub.publish(self.sg_200ghz_freq[i])
+            time.sleep(rate_pub)
+        time.sleep(rate)
+
+    def web_200ghz_power(self):
+        for i, pub in enumerate(self.pub_sg_200ghz_power):
+            pub.publish(self.sg_200ghz_power[i])
+            time.sleep(rate_pub)
+        time.sleep(rate)
+
+    def web_200ghz_onoff(self):
+        for i, pub in enumerate(self.pub_sg_200ghz_onoff):
+            if self.sg_200ghz_onoff[i] == 0: onoff = 'ON'
+            else: onoff = 'OFF'
+            pub.publish(onoff)
+            time.sleep(rate_pub)
+        time.sleep(rate)
+
+
+if __name__ == '__main__':
+    rospy.init_node(name)
+    sg_monitor()
+    rospy.spin()
--- web_monitor/scripts/monitor_sg.js
+++ web_monitor/scripts/monitor_sg.js
@@ -4,96 +4,181 @@ ros.on("connection", function() {console.log("websocket: connected"); });
 ros.on("error", function(error) {console.log("websocket error; ", error); });
 ros.on("close", function() {console.log("websocket: closed");});
 
-/*-- e8257d --*/
+/*-- 1st --*/
 
-var e8257d_freq = new ROSLIB.Topic({
+var 100ghz_1st_freq = new ROSLIB.Topic({
     ros : ros,
-    name : "/e8257d_freq",
+    name : "/100ghz_1st_freq",
     messageType : "std_msgs/Float64"
 });
-var e8257d_power = new ROSLIB.Topic({
+var 100ghz_1st_power = new ROSLIB.Topic({
     ros : ros,
-    name : "/e8257d_power",
+    name : "/100ghz_1st_power",
     messageType : "std_msgs/Float64"
 });
-var e8257d_onoff = new ROSLIB.Topic({
+var 100ghz_1st_onoff = new ROSLIB.Topic({
     ros : ros,
-    name : "/e8257d_onoff",
+    name : "/100ghz_1st_onoff",
+    messageType : "std_msgs/Int32"
+});
+
+var 200ghz_1st_freq = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_1st_freq",
+    messageType : "std_msgs/Float64"
+});
+var 200ghz_1st_power = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_1st_power",
+    messageType : "std_msgs/Float64"
+});
+var 200ghz_1st_onoff = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_1st_onoff",
+    messageType : "std_msgs/Int32"
+});
+
+/*-- 2nd upper --*/
+
+var 100ghz_2nd_upper_freq = new ROSLIB.Topic({
+    ros : ros,
+    name : "/100ghz_2nd_upper_freq",
+    messageType : "std_msgs/Float64"
+});
+var 100ghz_2nd_upper_power = new ROSLIB.Topic({
+    ros : ros,
+    name : "/100ghz_2nd_upper_power",
+    messageType : "std_msgs/Float64"
+});
+var 100ghz_2nd_upper_onoff = new ROSLIB.Topic({
+    ros : ros,
+    name : "/100ghz_2nd_upper_onoff",
+    messageType : "std_msgs/Int32"
+});
+
+var 200ghz_2nd_upper_freq = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_2nd_upper_freq",
+    messageType : "std_msgs/Float64"
+});
+var 200ghz_2nd_upper_power = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_2nd_upper_power",
+    messageType : "std_msgs/Float64"
+});
+var 200ghz_2nd_upper_onoff = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_2nd_upper_onoff",
+    messageType : "std_msgs/Int32"
+});
+
+/*-- 2nd lower --*/
+
+var 100ghz_2nd_lower_freq = new ROSLIB.Topic({
+    ros : ros,
+    name : "/100ghz_2nd_lower_freq",
+    messageType : "std_msgs/Float64"
+});
+var 100ghz_2nd_lower_power = new ROSLIB.Topic({
+    ros : ros,
+    name : "/100ghz_2nd_lower_power",
+    messageType : "std_msgs/Float64"
+});
+var 100ghz_2nd_lower_onoff = new ROSLIB.Topic({
+    ros : ros,
+    name : "/100ghz_2nd_lower_onoff",
+    messageType : "std_msgs/Int32"
+});
+
+var 200ghz_2nd_lower_freq = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_2nd_lower_freq",
+    messageType : "std_msgs/Float64"
+});
+var 200ghz_2nd_lower_power = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_2nd_lower_power",
+    messageType : "std_msgs/Float64"
+});
+var 200ghz_2nd_lower_onoff = new ROSLIB.Topic({
+    ros : ros,
+    name : "/200ghz_2nd_lower_onoff",
     messageType : "std_msgs/Int32"
 });
 
 
-e8257d_freq.subscribe(function(message) {
-    document.getElementById("e8257d_freq").innerHTML = (message.data);
+/*-- 100ghz --*/
+
+100ghz_1st_freq.subscribe(function(message) {
+    document.getElementById("100ghz_1st_freq").innerHTML = (message.data);
 });
 
-e8257d_power.subscribe(function(message) {
-    document.getElementById("e8257d_power").innerHTML = (message.data);
+100ghz_1st_power.subscribe(function(message) {
+    document.getElementById("100ghz_1st_power").innerHTML = (message.data);
 });
 
-e8257d_onoff.subscribe(function(message) {
-    document.getElementById("e8257d_onoff").innerHTML = (message.data);
+100ghz_1st_onoff.subscribe(function(message) {
+    document.getElementById("100ghz_1st_onoff").innerHTML = (message.data);
 });
 
-
-/*-- mg3692c --*/
-
-var mg3692c_freq = new ROSLIB.Topic({
-    ros : ros,
-    name : "/mg3692c_freq",
-    messageType : "std_msgs/Float64"
-});
-var mg3692c_power = new ROSLIB.Topic({
-    ros : ros,
-    name : "/mg3692c_power",
-    messageType : "std_msgs/Float64"
-});
-var mg3692c_onoff = new ROSLIB.Topic({
-    ros : ros,
-    name : "/mg3692c_onoff",
-    messageType : "std_msgs/Int32"
+100ghz_2nd_upper_freq.subscribe(function(message) {
+    document.getElementById("100ghz_2nd_upper_freq").innerHTML = (message.data);
 });
 
-
-mg3692c_freq.subscribe(function(message) {
-    document.getElementById("mg3692c_freq").innerHTML = (message.data);
+100ghz_2nd_upper_power.subscribe(function(message) {
+    document.getElementById("100ghz_2nd_upper_power").innerHTML = (message.data);
 });
 
-mg3692c_power.subscribe(function(message) {
-    document.getElementById("mg3692c_power").innerHTML = (message.data);
+100ghz_2nd_upper_onoff.subscribe(function(message) {
+    document.getElementById("100ghz_2nd_upper_onoff").innerHTML = (message.data);
 });
 
-mg3692c_onoff.subscribe(function(message) {
-    document.getElementById("mg3692c_onoff").innerHTML = (message.data);
+100ghz_2nd_lower_freq.subscribe(function(message) {
+    document.getElementById("100ghz_2nd_lower_freq").innerHTML = (message.data);
 });
 
-/*-- fsw0020 --*/
-
-var fsw0020_freq = new ROSLIB.Topic({
-    ros : ros,
-    name : "/fsw0020_freq",
-    messageType : "std_msgs/Float64"
-});
-var fsw0020_power = new ROSLIB.Topic({
-    ros : ros,
-    name : "/fsw0020_power",
-    messageType : "std_msgs/Float64"
-});
-var fsw0020_onoff = new ROSLIB.Topic({
-    ros : ros,
-    name : "/fsw0020_onoff",
-    messageType : "std_msgs/Int32"
+100ghz_2nd_lower_power.subscribe(function(message) {
+    document.getElementById("100ghz_2nd_lower_power").innerHTML = (message.data);
 });
 
-
-fsw0020_freq.subscribe(function(message) {
-    document.getElementById("fsw0020_freq").innerHTML = (message.data);
+100ghz_2nd_lower_onoff.subscribe(function(message) {
+    document.getElementById("100ghz_2nd_lower_onoff").innerHTML = (message.data);
 });
 
-fsw0020_power.subscribe(function(message) {
-    document.getElementById("fsw0020_power").innerHTML = (message.data);
+/*-- 200ghz --*/
+
+200ghz_1st_freq.subscribe(function(message) {
+    document.getElementById("200ghz_1st_freq").innerHTML = (message.data);
 });
 
-fsw0020_onoff.subscribe(function(message) {
-    document.getElementById("fsw0020_onoff").innerHTML = (message.data);
+200ghz_1st_power.subscribe(function(message) {
+    document.getElementById("200ghz_1st_power").innerHTML = (message.data);
+});
+
+200ghz_1st_onoff.subscribe(function(message) {
+    document.getElementById("200ghz_1st_onoff").innerHTML = (message.data);
+});
+
+200ghz_2nd_upper_freq.subscribe(function(message) {
+    document.getElementById("200ghz_2nd_upper_freq").innerHTML = (message.data);
+});
+
+200ghz_2nd_upper_power.subscribe(function(message) {
+    document.getElementById("200ghz_2nd_upper_power").innerHTML = (message.data);
+});
+
+200ghz_2nd_upper_onoff.subscribe(function(message) {
+    document.getElementById("200ghz_2nd_upper_onoff").innerHTML = (message.data);
+});
+
+200ghz_2nd_lower_freq.subscribe(function(message) {
+    document.getElementById("200ghz_2nd_lower_freq").innerHTML = (message.data);
+});
+
+200ghz_2nd_lower_power.subscribe(function(message) {
+    document.getElementById("200ghz_2nd_lower_power").innerHTML = (message.data);
+});
+
+200ghz_2nd_lower_onoff.subscribe(function(message) {
+    document.getElementById("200ghz_2nd_lower_onoff").innerHTML = (message.data);
 });
