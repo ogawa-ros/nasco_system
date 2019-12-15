b7a53608d27b1c240974b0c207d1f0e74c8724f6
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-12-15 03:34:19 +0000

    info : 20191215

---
+++ experiments/powermeter_monitor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+name = 'powermerter_monitor'
+
+import sys
+import rospy
+
+sys.path.append('/home/amigos/ros/src/nasco_system/scripts/')
+import nasco_controller
+import logger_controller
+import time
+import datetime
+import std_msgs.msg
+
+rospy.init_node(name)
+
+con = nasco_controller.controller(node=False)
+logger = logger_controller.logger()
+date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+dir_name_hot = name  + date + '.necstdb'
+
+logging_time = int(sys.argv[1]) # sec.
+
+#Hot
+con.slider0.set_step('u', 0)
+print('[INFO] : Hot')
+time.sleep(2.)
+
+
+logger.start(dir_name_hot)
+
+time.sleep(logging_time)
+logger.stop()
+
+#Cold
+#con.slider0.set_step('u', 250)
+#print('[INFO] : Finish')
--- scripts/set_sg.py
+++ scripts/set_sg.py
@@ -14,8 +14,8 @@ time.sleep(1)
 #con.sg_100ghz_2nd_upper.set_freq(9.5) # GHz 
 #con.sg_100ghz_2nd_lower.set_freq(4) # GHz
 con.sg_200ghz_1st.set_freq(18.814) # GHz                                                                                  
-con.sg_200ghz_2nd_upper.set_freq(4) # GHz                                                                                      
-con.sg_200ghz_2nd_lower.set_freq(6.6) # GHz   
+con.sg_200ghz_2nd_upper.set_freq(6.6) # GHz                                                                                      
+con.sg_200ghz_2nd_lower.set_freq(4.0) # GHz   
 time.sleep(1)
 
 con.sg_100ghz_1st.set_power(14) # dBm
