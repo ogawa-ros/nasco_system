5ebc04fecfebbdaeaa0e173146c7003d743c1e86
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-12-16 03:24:06 +0000

    20191216

--- scripts/set_sg.py
+++ scripts/set_sg.py
@@ -13,9 +13,9 @@ time.sleep(1)
 #con.sg_100ghz_1st.set_freq(17.4452003333) # GHz
 #con.sg_100ghz_2nd_upper.set_freq(9.5) # GHz 
 #con.sg_100ghz_2nd_lower.set_freq(4) # GHz
-con.sg_200ghz_1st.set_freq(18.814) # GHz                                                                                  
+con.sg_200ghz_1st.set_freq(18.764) # GHz                                                                                  
 con.sg_200ghz_2nd_upper.set_freq(6.6) # GHz                                                                                      
-con.sg_200ghz_2nd_lower.set_freq(4.0) # GHz   
+con.sg_200ghz_2nd_lower.set_freq(4.2) # GHz   
 time.sleep(1)
 
 con.sg_100ghz_1st.set_power(14) # dBm
--- scripts/set_sg_v2.py
+++ scripts/set_sg_v2.py
@@ -10,8 +10,8 @@ freq_2nd_upper_100ghz = 9.5
 freq_2nd_lower_100ghz = 4.0
 
 freq_1st_200ghz = 18.764
-freq_2nd_upper_200ghz = 4
-freq_2nd_lower_200ghz = 6.6
+freq_2nd_upper_200ghz = 6.6
+freq_2nd_lower_200ghz = 4.0
 
 # --- power params.
 power_1st_100ghz = 14
@@ -24,36 +24,36 @@ power_2nd_lower_200ghz = 14
 
 
 # OFF
-con.sg_100ghz_1st.set_onoff(0)
-con.sg_100ghz_2nd_upper.set_onoff(0)
-con.sg_100ghz_2nd_lower.set_onoff(0)
+#con.sg_100ghz_1st.set_onoff(0)
+#con.sg_100ghz_2nd_upper.set_onoff(0)
+#con.sg_100ghz_2nd_lower.set_onoff(0)
 con.sg_200ghz_1st.set_onoff(0)
 con.sg_200ghz_2nd_upper.set_onoff(0)
 con.sg_200ghz_2nd_lower.set_onoff(0)
 time.sleep(1)
 
 # set_freq
-con.sg_100ghz_1st.set_freq(freq_1st_100ghz)
-con.sg_100ghz_2nd_upper.set_freq(freq_2nd_upper_100ghz)
-con.sg_100ghz_2nd_lower.set_freq(freq_2nd_lower_100ghz)
+#con.sg_100ghz_1st.set_freq(freq_1st_100ghz)
+#con.sg_100ghz_2nd_upper.set_freq(freq_2nd_upper_100ghz)
+#con.sg_100ghz_2nd_lower.set_freq(freq_2nd_lower_100ghz)
 con.sg_200ghz_1st.set_freq(freq_1st_200ghz)
 con.sg_200ghz_2nd_upper.set_freq(freq_2nd_upper_200ghz)
 con.sg_200ghz_2nd_lower.set_freq(freq_2nd_lower_200ghz)
 time.sleep(1)
 
 # set_power
-con.sg_100ghz_1st.set_power(power_1st_100ghz)
-con.sg_100ghz_2nd_upper.set_power(power_2nd_upper_100ghz)
-con.sg_100ghz_2nd_lower.set_power(power_2nd_lower_100ghz)
-con.sg_200ghz_1st.set_power(power_1st_200ghz)
-con.sg_200ghz_2nd_upper.set_power(power_2nd_upper_200ghz)
-con.sg_200ghz_2nd_lower.set_power(power_2nd_lower_200ghz)
+#con.sg_100ghz_1st.set_power(power_1st_100ghz)
+#con.sg_100ghz_2nd_upper.set_power(power_2nd_upper_100ghz)
+#con.sg_100ghz_2nd_lower.set_power(power_2nd_lower_100ghz)
+#con.sg_200ghz_1st.set_power(power_1st_200ghz)
+#con.sg_200ghz_2nd_upper.set_power(power_2nd_upper_200ghz)
+#con.sg_200ghz_2nd_lower.set_power(power_2nd_lower_200ghz)
 time.sleep(1)
 
 # ON
-con.sg_100ghz_1st.set_onoff(1)
-con.sg_100ghz_2nd_upper.set_onoff(1)
-con.sg_100ghz_2nd_lower.set_onoff(1)
+#con.sg_100ghz_1st.set_onoff(1)
+#con.sg_100ghz_2nd_upper.set_onoff(1)
+#con.sg_100ghz_2nd_lower.set_onoff(1)
 con.sg_200ghz_1st.set_onoff(1)
 con.sg_200ghz_2nd_upper.set_onoff(1)
 con.sg_200ghz_2nd_lower.set_onoff(1)
